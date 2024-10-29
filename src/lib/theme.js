8f5f9eb651972e15dd817cd0fa8251b1ea20ce4f
scapula07 <bartholomewonogwu@yahoo.com>
2024-10-29 21:07:56 +0100

    remove chakra ui

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -1,7 +1,7 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react';
 
 const config = {
-  initialColorMode: "dark", // 'dark' | 'light'
+  initialColorMode: 'light',
   useSystemColorMode: false,
 };
 
@@ -9,44 +9,10 @@ const theme = extendTheme({
   config,
   colors: {
     primary: {
-      100: "#F1A7A7",
-      200: "#EC8383",
-      300: "#E66060",
-      400: "#E13D3D",
-      500: "#C71F1F",
-      600: "#B01C1C",
-      700: "#8D1616",
-      800: "#6A1111",
-      900: "#460B0B",
-    },
-    secondary: {
-      100: "#EEECEC",
-    },
-    secondaryDark: {
-      100: "#141414",
-    },
-    blue: {
-      200: "#aaaaaa",
-    },
-    gray: {
-      300: "#aaaaaa",
-    },
-  },
-  components: {
-    Button: {
-      defaultProps: {
-        colorScheme: "primary",
-      },
-    },
-    Text: {
-      baseStyle: {
-        color: "gray.800",
-      },
-    },
-    Link: {
-      baseStyle: {
-        color: "primary.500",
-      },
+      100: "#E2E8F0",
+      200: "#CBD5E0",
+      300: "#A0AEC0",
+      // Add more shades as needed
     },
   },
 });
