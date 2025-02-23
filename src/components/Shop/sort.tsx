9364916f94fb9ace1c9530d23d04be1cf5f34800
scapula07 <bartholomewonogwu@yahoo.com>
2025-02-23 10:14:32 +0100

    filter categories, find prices

--- src/components/Shop/sort.tsx
+++ src/components/Shop/sort.tsx
@@ -5,24 +5,9 @@ import { FaList } from "react-icons/fa";
 export default function Sort({count}:{count:number}) {
   return (
         <div className='w-full flex justify-between'>
-              <div className='w-1/2 flex items-center space-x-10'>
-                   <div className='flex space-x-2 items-center'>
-                        <BsGrid
-                           className=''
-                        />
-                      <p className='font-light text-lg'>Grid</p>
-                   </div>
-                   <div className='flex space-x-2 items-center'>
-                       <FaList
-                          className=''
-                       />
-                      <p className=' font-light text-lg'>List</p>
-                   </div>
-              </div>
-             <div className='w-1/2 flex items-center space-x-8'>
-                  <select className='border py-3 px-6 rounded-lg text-sm font-semibold text-slate-600 bg-white'>
-                      <option>Sort by</option>
-                  </select>
+ 
+             <div className='w-full flex items-center space-x-8 justify-end'>
+             
                   <p className='font-semibold'>Showing 1-9 of {count} results</p>     
              </div>
         </div>
